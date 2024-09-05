8610ac313e4ca6652e0cf903c874c09579d6ac7a
Deepak Jaiswal <djdeepakjaiswal1996@gmail.com>
2024-09-05 07:14:57 +0530

    added condition check

--- src/gitlearning/GitStudy.java
+++ src/gitlearning/GitStudy.java
@@ -2,7 +2,13 @@ package gitlearning;
 
 public class GitStudy {
 	public static void main(String[] args) {
+		String mood = "happy";
+		if(mood == "happy") {
 		System.out.println("Learning Git Throughly");
+	}
+		else {
+			System.out.println("sleep");
+		}
 	}
 
 }
